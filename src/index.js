5122fa1961ba490f132d340c95a5a574e41d8185
Andrey Krainov <metal.awp@gmail.com>
2021-03-13 13:21:47 +0300

    init: start Towelsort task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if (matrix === undefined || matrix.length == 0) {
+        return [];
+    } else {
+        for (let i = 0; i < matrix.length; i++) {
+            i % 2 !== 0 ? matrix[i].reverse() : false;
+        }
+    }
+    return matrix.join().split(",");
+};
